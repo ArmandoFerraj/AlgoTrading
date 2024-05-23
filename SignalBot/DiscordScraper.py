21338239e8f086a145b0eab1b6ab0359b48f6a9b
Armando <aferraj22@gmail.com>
2024-05-22 21:06:12 -0400

    Update DiscordScraper.py

--- SignalBot/DiscordScraper.py
+++ SignalBot/DiscordScraper.py
@@ -10,19 +10,15 @@ class DiscordScraper(discord.Client):
         print('CoinBot in the house!')
     
     async def on_message(self, message):
-        raw_message = {"message": [(str(message.content))]} #(str(message.author), str(message.content)) 
+        raw_message = {"message": (str(message.content))} 
 
         async with httpx.AsyncClient() as client:
             response = await client.post(url, json=raw_message)
             return response.json()
        
-        
-
 intents = discord.Intents.default()
 intents.messages = True
-
 intents.message_content = True  
-
 client = DiscordScraper(intents=intents)
+
 client.run(token)
- 
