5fc85266ceecfa7b9acc7caba20924faa78153a5
Armando <aferraj22@gmail.com>
2024-05-22 21:05:31 -0400

    Update server.py

--- SignalBot/server.py
+++ SignalBot/server.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 message_list = []
@@ -7,25 +7,27 @@ message_list = []
 app = FastAPI()
 
 class DiscordData(BaseModel):
-    message: list[str] #Tuple[str, str]
+    message: str #Tuple[str, str]
 
 @app.post("/postmessage/")
 async def from_discord(data: DiscordData):
     message_list.append(data.message)
-    print('Discord',message_list)
     return message_list
 
 @app.get("/getmessage/")
 async def to_signalbot():
     global message_list
-    current_message = message_list 
-    message_list = [] #empty message list after each GET request
-    return current_message#{"message":current_message}
-
+    if len(message_list) == 0:
+        print('EMPTY NO MESSAGES FROM DISCORD')
+        raise HTTPException(status_code=204, detail="No content")
+    
+    else:
+        current_message = message_list  #create this temp list that sends the message 
+        message_list = [] #empty message list after each GET request so we dont ever duplicate messages
+        return current_message
 
 def start_server():
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
 if __name__ == "__main__":
     start_server()
- 
