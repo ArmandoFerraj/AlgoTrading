22a77b67f4cec2eed36a5475202d86ff17ac00b9
Armando <aferraj22@gmail.com>
2024-05-24 19:37:26 -0400

    Add files via upload
    outline for the state machine

---
+++ SignalBot/StateMachine.py
@@ -0,0 +1,35 @@
+STATE_BUY = "BUY"
+STATE_SELL = "SELL"
+
+class Statemachine:
+    def __init__(self):
+        self.state = STATE_BUY
+
+    def enter_position(self, trading_signal):
+        # trading_signal is an element from signals. it should have all of the information needed to enter a trade ie: ticker, long/short
+        #send request to coinbase to enter a position
+        self.state = STATE_SELL #switch state
+
+    def exit_position(self):
+        #send request to coinbase to exit position
+        self.state = STATE_BUY #switch state
+
+    def run(self, signal_list): #pass in signals as the parameter
+        if self.state == STATE_BUY:
+            if signal_list:
+                signal = signal_list.pop(0)# takes first signal from the list
+                self.enter_position(signal) 
+                return signal_list #return the list so that the signal we just used is gone
+            else:
+                print("no signals yet")
+        elif self.state == STATE_SELL:
+            # this is where our exit strategy logic should be
+            self.exit_position()
+            
+    
+
+
+
+
+
+
