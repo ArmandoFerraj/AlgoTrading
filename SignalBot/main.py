7a9cf5ac74b29086fa5c52949264ed2fcf0f79b9
Armando <aferraj22@gmail.com>
2024-05-28 14:56:48 -0400

    Update main.py

--- SignalBot/main.py
+++ SignalBot/main.py
@@ -1,37 +1,15 @@
-import requests
+import helper
 from  StateMachine import FSM
 
 url = 'http://localhost:8000/getmessage/'
 signals = []
-
 SIGNALBOT = FSM()
-    
-def process_messages(message): #classifies each element as a trading signal or not, returns a list of all trading signals
-    return message
-      
-def get_messages(url): #gets response from the server and processes all messages
-    response = requests.get(url)
-    if response.status_code == 200:
-        incoming_message = response.json()
-        return process_messages(incoming_message)
-    
-    elif response.status_code == 204:
-        # print("No new messages")
-        return None
-    else:
-        print(f"Failed to fetch messages, status code: {response.status_code}")
-        return None
-
-def append_signal(signal_list, new_list):
-    if new_list:
-            signal_list.extend(new_list)
-
 
 while True:
-    incoming_signals = get_messages(url)
-    append_signal(signals, incoming_signals)
+    incoming_signals = helper.get_messages(url)
+    helper.append_signal(signals, incoming_signals)
+    print(f"These are all of our trading signals{signals}")
     SIGNALBOT.run(signals)
-
 
 
 
